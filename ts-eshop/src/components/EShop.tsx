18c9d7d87388f8d6fb557645667d895f5d6e5305
Leonid Kudryashov <leoural66@yandex.ru>
2022-05-07 16:40:39 +0500

    типизация компонентов

--- ts-eshop/src/components/EShop.tsx
+++ ts-eshop/src/components/EShop.tsx
@@ -1,26 +1,31 @@
 import React, { Component } from "react";
+import { ContainerBox } from './ContainerBox'
 
 type CounterState = {
     show: boolean,
 }
 
 type CounterProps = {
-
 }
 
 export default class EShop extends Component<CounterProps, CounterState>{
     constructor(props: CounterProps) {
         super(props)
-
         this.state = {
             show: true,
         }
     }
 
-    render(): React.ReactNode {
+    componentDidMount(): void {
+    }
+
+    render() {
         return (
             <>
                 <div> ПРИВЕТ TS REACT!!!!!! </div>
+                <ContainerBox
+                    text="test TS + REACT props in function"
+                />
             </>
         )
     }
