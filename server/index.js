a001de0f4c9fb73eeadb4cb2d8aa17ac05ff565f
Leonid Kudryashov <leoural66@yandex.ru>
2022-09-18 13:47:19 +0500

    start migrations

--- server/index.js
+++ server/index.js
@@ -3,6 +3,19 @@ const express = require('express');
 const cors = require('cors');
 const crypto = require("crypto");
 const { nanoid } = require("nanoid"); // поддерживается v3.1.30
+
+const knex = require('knex')({
+    client: 'pg',
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    },
+});
+
+
 const login = require("./src/login/login");
 const registration = require("./src/login/registration");
 
@@ -13,10 +26,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/login', login)
-
 app.post('/registration', registration)
-
-
 
 app.listen(PORT, () => {
     console.log(`SERVER start to PORT: ${PORT}`);
--- server/src/login/registration.js
+++ server/src/login/registration.js
@@ -6,6 +6,9 @@ module.exports = async (req, res) => {
     console.log(`key::: `, key);
     console.log(`DATA::: `, data);
 
+    //todo 1: сформировать HASH пароля
+    //todo 2: сформировать новый объект для БД
+    //todo 3: Сохранить нового пользователя в БД
 
     res.json({ server: 'REGISTRATION: response - ok' })
 }
